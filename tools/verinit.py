422aacadd2dec36e9dae42eb58df253e587721eb
Howard Mao <zhehao.mao@gmail.com>
2013-06-23 18:30:01 -0700

    add script to generate verilog initializations

---
+++ tools/verinit.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# Reads values line by line on standard input and generates
+# A verilog initialization on standard output
+
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        varname = 'data'
+    else:
+        varname = sys.argv[1]
+
+    print("initial begin")
+
+    for i, line in enumerate(sys.stdin):
+        code = "\t%s[%d] = %s;" % (varname, i, line.strip())
+        print(code)
+
+    print("end")
