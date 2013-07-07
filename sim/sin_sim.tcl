5cc11b84607322ed0641d03eaf87e216aee0e40d
Howard Mao <zhehao.mao@gmail.com>
2013-07-06 18:38:29 -0700

    add more signals to sin testbench

--- sim/sin_sim.tcl
+++ sim/sin_sim.tcl
@@ -3,10 +3,25 @@ quietly WaveActivateNextPane {} 0
 add wave -noupdate /sin_tb/clk
 add wave -noupdate /sin_tb/reset
 add wave -noupdate /sin_tb/result
+add wave -noupdate /sin_tb/done
 add wave -noupdate /sin_tb/sinunit/theta
 add wave -noupdate /sin_tb/sinunit/prec
 add wave -noupdate /sin_tb/sinunit/state
 add wave -noupdate /sin_tb/sinunit/coeffind
+add wave -noupdate /sin_tb/sinunit/lastind
+add wave -noupdate /sin_tb/sinunit/square
+add wave -noupdate /sin_tb/sinunit/last_power
+add wave -noupdate /sin_tb/sinunit/term_power
+add wave -noupdate /sin_tb/sinunit/accum
+add wave -noupdate /sin_tb/sinunit/coeff
+add wave -noupdate /sin_tb/sinunit/accum_next
+add wave -noupdate /sin_tb/sinunit/power_res
+add wave -noupdate /sin_tb/sinunit/term_res
+add wave -noupdate /sin_tb/sinunit/add_res
+add wave -noupdate /sin_tb/sinunit/alldone
+add wave -noupdate /sin_tb/sinunit/power_mult_rst
+add wave -noupdate /sin_tb/sinunit/term_mult_rst
+add wave -noupdate /sin_tb/sinunit/add_rst
 TreeUpdate [SetDefaultTree]
 WaveRestoreCursors {{Cursor 1} {245000 ps} 0}
 quietly wave cursor active 1
@@ -26,4 +41,4 @@ configure wave -timelineunits ns
 update
 WaveRestoreZoom {0 ps} {579744 ps}
 
-run 1200 ns
+run 10 us
