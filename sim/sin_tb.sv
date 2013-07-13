8adf5a31f15581ca9fa2607dfeeea9906c8c6560
Howard Mao <zhehao.mao@gmail.com>
2013-07-13 10:03:08 -0700

    make sure to test sin for negative angles

--- sim/sin_tb.sv
+++ sim/sin_tb.sv
@@ -1,8 +1,8 @@
 module sin_tb ();
 
-reg [31:0] theta [0:2];
-reg [3:0] prec [0:2];
-reg [31:0] expected [0:2];
+reg [31:0] theta [0:3];
+reg [3:0] prec [0:3];
+reg [31:0] expected [0:3];
 reg clk = 1'b1;
 reg reset;
 wire [31:0] result;
@@ -24,20 +24,23 @@ initial begin
 	theta[0] = 32'h00000000;
 	theta[1] = 32'h3f800000;
 	theta[2] = 32'h3f99999a;
-	prec[0] = 4'hA;
+	theta[3] = 32'hbfa8f5c3;
+	prec[0] = 4'ha;
 	prec[1] = 4'h9;
 	prec[2] = 4'h7;
+	prec[3] = 4'h8;
 	expected[0] = 32'h00000000;
 	expected[1] = 32'h3f576aa4;
 	expected[2] = 32'h3f6e9a1d;
+	expected[3] = 32'hbf77fdb5;
 
-	for (ind = 0; ind < 3; ind++) begin
+	for (ind = 0; ind < 4; ind++) begin
 		reset = 1;
 		#20000 reset = 0;
 		while (done == 0) begin
 			#20000;
 		end
-		assert (result[31:3] == expected[ind][31:3]);
+		assert (result[31:4] == expected[ind][31:4]);
 	end
 end
 
