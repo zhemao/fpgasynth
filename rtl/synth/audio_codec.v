4c1aa30f0b25993ee25cf1bf10edab0a1c567405
Howard Mao <zhehao.mao@gmail.com>
2013-07-13 09:34:10 -0700

    more flexible channel selection for audio_codec

--- rtl/synth/audio_codec.v
+++ rtl/synth/audio_codec.v
@@ -4,7 +4,7 @@ module audio_codec (
     sample_end,
     audio_output,
     audio_input,
-    mono_stereo,
+    channel_sel,
 
     AUD_ADCLRCK,
     AUD_ADCDAT,
@@ -18,8 +18,8 @@ input reset_n;
 output sample_end;
 input [15:0] audio_output;
 output reg [15:0] audio_input;
-// 1 is stereo, 0 is mono
-input mono_stereo;
+// 1 - left, 0 - right
+input [1:0] channel_sel;
 
 output AUD_ADCLRCK;
 input AUD_ADCDAT;
@@ -37,9 +37,7 @@ assign lrck = !lrck_divider[7];
 
 reg [15:0] shift_out;
 reg [15:0] shift_temp;
-reg [15:0] shift_in_left;
-reg [15:0] shift_in_right;
-wire [15:0] shift_in_avg = shift_in_left[15:1] + shift_in_right[15:1];
+reg [15:0] shift_in;
 
 assign AUD_ADCLRCK = lrck;
 assign AUD_DACLRCK = lrck;
@@ -56,50 +54,34 @@ always @(posedge clk) begin
     end
 end
 
-assign sample_end = (lrck_divider == 8'h7e && mono_stereo == 1'b1 || 
-                        lrck_divider == 8'hfe) ? 1'b1 : 1'b0;
+wire left_chan_end = (lrck_divider == 8'h7e) ? 1'b1 : 1'b0;
+wire right_chan_end = (lrck_divider == 8'hfe) ? 1'b1 : 1'b0;
+
+assign sample_end = ((left_chan_end & channel_sel[1]) | 
+                        (right_chan_end & channel_sel[0]));
 
 wire clr_lrck = (lrck_divider == 8'h7f) ? 1'b1 : 1'b0;
 wire set_lrck = (lrck_divider == 8'hff) ? 1'b1 : 1'b0;
 wire set_bclk = (bclk_divider == 3'b011) ? 1'b1 : 1'b0;
 wire clr_bclk = (bclk_divider == 3'b111) ? 1'b1 : 1'b0;
 
-always @(*) begin
-    if (mono_stereo == 1) begin
-        if (lrck == 1) begin
-            audio_input <= shift_in_left;
-        end else begin
-            audio_input <= shift_in_right;
-        end
-    end else begin
-        audio_input <= shift_in_avg;
-    end
-end
-
 always @(posedge clk) begin
     if (reset_n == 0) begin
         shift_out <= 16'h0;
-        shift_in_left <= 16'h0;
-        shift_in_right <= 16'h0;
-    end else if (set_lrck == 1) begin
-        shift_out <= audio_output;
-        shift_temp <= audio_output;
-        shift_in_left <= 16'h0;
-        shift_in_right <= 16'h0;
-    end else if (clr_lrck == 1) begin
-        if (mono_stereo == 1) begin
-            // if stereo, pull from audio_output again
+        shift_in <= 16'h0;
+        shift_in <= 16'h0;
+    end else if ((set_lrck | clr_lrck) == 1'b1) begin
+        // check if current channel is selected
+        if (channel_sel[set_lrck] == 1'b1) begin
             shift_out <= audio_output;
-        end else begin
-            // if mono, restore from shift_temp
-            shift_out <= shift_temp;
-        end
+            shift_temp <= audio_output;
+            shift_in <= 16'h0;
+            audio_input <= shift_in;
+        end else shift_out <= shift_temp;
     end else if (set_bclk == 1) begin
-        if (lrck == 1) begin
-            shift_in_left <= {shift_in_left[14:0], AUD_ADCDAT};
-        end else begin
-            shift_in_right <= {shift_in_right[14:0], AUD_ADCDAT};
-        end
+        // only read in if channel is selected
+        if (channel_sel[lrck] == 1'b1)
+            shift_in <= {shift_in[14:0], AUD_ADCDAT};
     end else if (clr_bclk == 1) begin
         shift_out <= {shift_out[14:0], 1'b0};
     end
