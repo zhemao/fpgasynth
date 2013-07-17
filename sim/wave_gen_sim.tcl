9517786d90c139ab6d6fc2d2986aa4ccf5d4fa38
Howard Mao <zhehao.mao@gmail.com>
2013-07-16 19:18:27 -0700

    use tcl script to set up wave_gen_tb

---
+++ sim/wave_gen_sim.tcl
@@ -0,0 +1,31 @@
+onerror {resume}
+quietly WaveActivateNextPane {} 0
+add wave -noupdate /wave_gen_tb/clk
+add wave -noupdate /wave_gen_tb/reset
+add wave -noupdate /wave_gen_tb/aud_req
+add wave -noupdate /wave_gen_tb/aud_data
+add wave -noupdate /wave_gen_tb/aud_done
+add wave -noupdate /wave_gen_tb/generator/theta
+add wave -noupdate /wave_gen_tb/generator/base_samp
+add wave -noupdate /wave_gen_tb/generator/scaled_samp
+add wave -noupdate /wave_gen_tb/generator/next_samp
+TreeUpdate [SetDefaultTree]
+WaveRestoreCursors {{Cursor 1} {245000 ps} 0}
+quietly wave cursor active 1
+configure wave -namecolwidth 206
+configure wave -valuecolwidth 100
+configure wave -justifyvalue left
+configure wave -signalnamewidth 0
+configure wave -snapdistance 10
+configure wave -datasetprefix 0
+configure wave -rowmargin 4
+configure wave -childrowmargin 2
+configure wave -gridoffset 0
+configure wave -gridperiod 1
+configure wave -griddelta 40
+configure wave -timeline 0
+configure wave -timelineunits ns
+update
+WaveRestoreZoom {0 ps} {579744 ps}
+
+run 3840 us
