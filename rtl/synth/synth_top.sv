2e2a49dd962663e19320d668cf81dd11ee45646c
Howard Mao <zhehao.mao@gmail.com>
2013-07-19 21:25:08 -0700

    implement oscillator summation state machine

--- rtl/synth/synth_top.sv
+++ rtl/synth/synth_top.sv
@@ -46,4 +46,46 @@ for (ind = 0; ind < 4; ind = ind + 1)
 end
 endgenerate
 
+reg [2:0] step;
+
+reg [15:0] arga;
+reg [15:0] argb;
+wire [15:0] sum_res = arga + argb;
+reg [15:0] sum_temp;
+
+always @(posedge clk) begin
+	if (reset == 1'b1) begin
+		step <= 2'b0;
+	end else case (step)
+		0: if (aud_req == 1'b1) begin
+			aud_data <= sum_temp;
+			osc_next <= 1'b1;
+			step <= 3'd1;
+		end
+		1: begin
+			osc_next <= 1'b0;
+			if (osc_done == 4'b1111) begin
+				arga <= osc_output[0];
+				argb <= osc_output[1];
+				step <= 3'd2;
+			end
+		end
+		2: begin
+			sum_temp <= sum_res;
+			arga <= osc_output[2];
+			argb <= osc_output[3];
+			step <= 3'd3;
+		end
+		3: begin
+			arga <= sum_temp;
+			argb <= sum_res;
+			step <= 3'd4;
+		end
+		4: begin
+			sum_temp <= sum_res;
+			step <= 3'd0;
+		end
+	endcase
+end
+
 endmodule
